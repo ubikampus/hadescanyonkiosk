c14fad48a76d6578627151667f7d4b3865942492
Petri Savolainen <petri.savolainen@hiit.fi>
2019-03-05 07:45:09 +0200

    Several bugfixes

--- setuphadescanyonkiosk.sh
+++ setuphadescanyonkiosk.sh
@@ -10,11 +10,13 @@ sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw --force enable
 
+sudo apt-get install -y software-properties-common
+
 sudo add-apt-repository 'deb http://dl.google.com/linux/chrome/deb/ stable main'
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo add-apt-repository universe
+sudo add-apt-repository -y universe
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends xorg openbox google-chrome-stable pulseaudio unclutter
+sudo apt-get install -y --no-install-recommends xorg openbox google-chrome-stable pulseaudio unclutter 
 
 
 #Update kernel and install graphics drivers
@@ -25,16 +27,24 @@ sudo apt-get install -y --no-install-recommends xorg openbox google-chrome-stabl
 # Ideally you'd read this, understand it, and run each step by hand, carefully.
 # But it worked first try for me as is.
 set -ex
-mkdir tmp
+mkdir -p tmp
 cd tmp
 # New mesa (ca. 18.1.5) and friends
-sudo add-apt-repository ppa:ubuntu-x-swat/updates
-sudo apt dist-upgrade        # pulls new mesa from above ppa
+sudo add-apt-repository -y ppa:ubuntu-x-swat/updates
+sudo apt dist-upgrade -y       # pulls new mesa from above ppa
+
 # New linux kernel (preview of 4.19)
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19-rc2/linux-headers-4.19.0-041900rc2_4.19.0-041900rc2.201809022230_all.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19-rc2/linux-headers-4.19.0-041900rc2-generic_4.19.0-041900rc2.201809022230_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19-rc2/linux-image-unsigned-4.19.0-041900rc2-generic_4.19.0-041900rc2.201809022230_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19-rc2/linux-modules-4.19.0-041900rc2-generic_4.19.0-041900rc2.201809022230_amd64.deb
+#wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19-rc2/linux-headers-4.19.0-041900rc2_4.19.0-041900rc2.201809022230_all.deb
+#wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19-rc2/linux-headers-4.19.0-041900rc2-generic_4.19.0-041900rc2.201809022230_amd64.deb
+#wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19-rc2/linux-image-unsigned-4.19.0-041900rc2-generic_4.19.0-041900rc2.201809022230_amd64.deb
+#wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19-rc2/linux-modules-4.19.0-041900rc2-generic_4.19.0-041900rc2.201809022230_amd64.deb
+
+
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.26/linux-headers-4.19.26-041926_4.19.26-041926.201902270533_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.26/linux-headers-4.19.26-041926-generic_4.19.26-041926.201902270533_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.26/linux-image-unsigned-4.19.26-041926-generic_4.19.26-041926.201902270533_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.26/linux-modules-4.19.26-041926-generic_4.19.26-041926.201902270533_amd64.deb
+
 sudo dpkg -i linux-*.deb
 # New linux-firmware (will be released as 1.175 or something like that)
 wget -m -np https://people.freedesktop.org/~agd5f/radeon_ucode/vegam/
@@ -47,7 +57,7 @@ rm -rf tmp
 
 sudo apt-get install -y xserver-xorg-legacy
 
-sudo cat > /etc/X11/Xwrapper.config << EOL
+sudo tee /etc/X11/Xwrapper.config << EOL
 allowed_users=anybody
 needs_root_rights=yes
 EOL
@@ -58,7 +68,7 @@ sudo useradd --create-home --shell /bin/bash kiosk
 
 #create file /home/kiosk/kioskx11.sh with the following content
 
-sudo cat > /home/kiosk/kioskx11.sh << EOL
+sudo tee /home/kiosk/kioskx11.sh << EOL
 #!/bin/bash
 
 unclutter -idle 0.001 -root
@@ -77,7 +87,7 @@ sudo chmod u+x /home/kiosk/kioskx11.sh
 
 #create file /home/kiosk/runkiosk.sh with the following content
 
-sudo cat > /home/kiosk/runkiosk.sh << EOL
+sudo tee /home/kiosk/runkiosk.sh << EOL
 /usr/bin/startx /etc/X11/Xsession ./kioskx11.sh
 EOL
 
@@ -86,7 +96,7 @@ sudo chmod u+x /home/kiosk/runkiosk.sh
 
 #create file /etc/systemd/system/kiosk.service with the following content
 
-sudo cat > /etc/systemd/system/kiosk.service << EOL
+sudo tee /etc/systemd/system/kiosk.service << EOL
 [Unit]
 Description=Kiosk
 
@@ -108,18 +118,18 @@ sudo systemctl enable /etc/systemd/system/kiosk.service
 
 #Enable automatic security updates
 
-sudo apt-get install unattended-upgrades
+sudo apt-get install -y unattended-upgrades
 
 # only append, do not everwrite!
 
-sudo cat >> /etc/apt/apt.conf.d/50unattended-upgrades << EOL
+sudo bash -c 'cat >> /etc/apt/apt.conf.d/50unattended-upgrades << EOL
 Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";
 Unattended-Upgrade::Remove-Unused-Dependencies "true";
 Unattended-Upgrade::Automatic-Reboot "true";
 Unattended-Upgrade::Automatic-Reboot-Time "03:38";
-EOL
+EOL'
 
-sudo cat > /etc/apt/apt.conf.d/20auto-upgrades << EOL
+sudo tee /etc/apt/apt.conf.d/20auto-upgrades << EOL
 APT::Periodic::Update-Package-Lists "7";
 APT::Periodic::Download-Upgradeable-Packages "7";
 APT::Periodic::AutocleanInterval "7";
